e8e50e3ea82909e9d7923da3aeba0b4363582889
Hadi Ka (Nystrex) <alcacounihadi@gmail.com>
2022-07-25 10:50:29 -0400

    missing functions for windows

--- manager/root_windows.go
+++ manager/root_windows.go
@@ -10,3 +10,11 @@ import (
 func GetLauncherRoot() string {
 	return filepath.Join(os.Getenv("APPDATA"), ".genecraft", "launcher")
 }
+
+func GetLibraryPath() string {
+	return filepath.Join(GetLauncherRoot(), "libs")
+}
+
+func GetAssetsPath() string {
+	return filepath.Join(GetLauncherRoot(), "assets")
+}
