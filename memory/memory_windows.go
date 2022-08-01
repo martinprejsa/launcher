3a5cf4d2682e0f3d73ed27f922e3e83789e62527
Martin Prejsa <martin.prejsa1@gmail.com>
2022-08-01 13:32:39 +0200

    Finished memory info for windows

--- memory/memory_windows.go
+++ memory/memory_windows.go
@@ -2,11 +2,19 @@
 
 package memory
 
+import (
+	"syscall"
+	"unsafe"
+)
+
 func GetMemoryTotal() int {
 	var mod = syscall.NewLazyDLL("kernel32.dll")
 	var proc = mod.NewProc("GetPhysicallyInstalledSystemMemory")
-	var mem uint64
+	var mem uint64 = 0
 
-	ret, _, _ := proc.Call(uintptr(unsafe.Pointer(&mem)))
-	return mem
+	ret, _, _ = proc.Call(uintptr(unsafe.Pointer(&mem)))
+	if ret != 1 || mem == 0 {
+		return -1
+	}
+	return int(mem)
 }
